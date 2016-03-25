32cb91f534a652f0a61b22ef3b38465fc53d90f1
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2016-03-25 15:18:59 -0400

    Initial Commit

---
+++ SNMPKit/SNMPKit.h
@@ -0,0 +1,19 @@
+//
+//  SNMPKit.h
+//  SNMPKit
+//
+//  Created by Chris Rittersdorf on 3/25/16.
+//  Copyright © 2016 Manlycode Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SNMPKit.
+FOUNDATION_EXPORT double SNMPKitVersionNumber;
+
+//! Project version string for SNMPKit.
+FOUNDATION_EXPORT const unsigned char SNMPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SNMPKit/PublicHeader.h>
+
+
