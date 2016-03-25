0660fa3422c4c802f74fbff1b171d07931837cf4
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2016-03-25 15:52:19 -0400

    Add targets

--- SNMPKit/SNMPKit.h
+++ SNMPKit/SNMPKit.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Manlycode Industries. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SNMPKit.
 FOUNDATION_EXPORT double SNMPKitVersionNumber;
